93cf5da66de50fbfd99e7ee1c34e167a91fc7624
Anton Shvets <antshvets@gmail.com>
2018-05-20 11:18:53 +0300

    - | api.service | unused import

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as theMovieDb from 'themoviedb-javascript-library';
 import { Observable, Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
